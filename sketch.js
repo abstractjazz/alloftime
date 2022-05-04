7b90d1a9c584b2b4d52e2c39fafe445c6e25de4d
github username <evan@heyevan.com>
2022-05-04 11:15:40 -0700

    initial commit

---
+++ sketch.js
@@ -0,0 +1,125 @@
+
+var boxSz = 2400;
+var numSpheres = 3000;
+let numPlanets = 11;
+//declare empty arrays for x,y,z positions
+var x = [];
+var y = [];
+var z = [];
+let myFont;
+let randomColorVal;
+let from;
+let to;
+let interA;
+let song;
+pointX = 0;
+pointY = 0;
+let songVal;
+let colorStartR;
+let colorStartG;
+let colorStartB;
+let s;
+let colorGauge;
+const space=select('.p5Canvas')
+
+
+
+
+function preload(){
+  myFont=loadFont('./RedHatMono.ttf')
+  song=loadSound('./audio/xylophone.mp3')
+}
+
+function setup() {
+
+  frameRate(60)
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  background(0);
+  song.loop();
+  randomColorVal = Math.random()*255
+  to=color(237, 181, 252)
+  from=color(0, 67, 255)
+  interA = lerpColor(from, to, 3);
+  colorStartR=19
+  colorStartG=105
+  colorStartB=211
+  
+
+  for (var i = 0; i < numSpheres; i++) {
+    //assigns random value to positions 0 - end of numSpheres length (in this case 3000)
+    x[i] = random(-boxSz, boxSz);
+    y[i] = random(-boxSz, boxSz);
+    z[i] = random(-boxSz, boxSz);
+  }
+
+  textFont(myFont)
+  textSize(36)
+  textAlign(CENTER,CENTER)
+}
+
+function draw() {
+  canvas.onclick=function(){colorChange}
+  let volume = map(height-mouseY, 0, width, 0, 1);
+  background(0)
+  song.amp(volume)
+ 
+  orbitControl(2.5,2.5,0.009)
+  stroke(255);
+
+  stroke('#fff')
+  strokeWeight(1)
+  point(pointX*random, pointY*random())
+ 
+  //back
+  
+
+  for (var i = 0; i < numSpheres; i++) {
+    //push and pop are used together to save & then restore styles
+    push();
+    //translate displaces the x,y,z positions of points (creating depth of star effect
+    translate(x[i], y[i], z[i])
+    stroke(random()*255, random()*255, random()*255)
+    strokeWeight(0.75)
+    point(boxSz / 50, 8);
+    pop();
+  }
+
+
+  for (let j=0; j < numPlanets; j++ ) { 
+  // stroke('#fff')
+  noStroke()
+  strokeWeight(0.25)
+  
+  fill(`rgba(${colorStartR}, ${colorStartG}, ${colorStartB},0.15)`)
+  
+  rotateY(frameCount * 0.0001);
+  sphere(boxSz/5, mouseY)
+  sphere(boxSz/15, mouseX)
+  sphere(boxSz/10, 8)
+  sphere(boxSz/8, 8)
+  sphere(boxSz/44, mouseX)
+  sphere(boxSz/38, 8)
+  sphere(boxSz/24, 8)
+  sphere(boxSz/50, mouseY)
+  sphere(boxSz/120, mouseX)
+  sphere(boxSz/70, 8)
+  }
+
+  fill(interA)
+  text('Who Are You In All Of Time?', windowWidth/2, windowHeight/2.25)
+ 
+
+}
+
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
+function colorChange() {
+  colorStartR+=random()*120
+  colorStartG+=random()*120
+  colorStartB+=random()*120
+}
+
